65cbb5d6d3cd9cac2d8cff5dd7117998d366c84d
MaartenBorsje <59605923+MaartenBorsje@users.noreply.github.com>
2022-08-09 20:01:56 +0200

    Initial Commit

---
+++ CoreDataExample/CoreDataExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataExampleApp.swift
+//  CoreDataExample
+//
+//  Created by Maarten Borsje on 09/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
